553467a5b87e701549ddd6648ace8f3010547dda
kunnikarB <kunnikarboon@gmail.com>
2024-10-31 09:46:19 +0100

    Generate HTML for checkout page

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,13 @@
-export const cart = [];
+export const cart = [
+  {
+    productId: 1,
+    quantity: 2,
+  },
+  {
+    productId: 2,
+    quantity: 1,
+  }
+];
 
 // Add to cart functionality 
  export function addToCart(productId) {
